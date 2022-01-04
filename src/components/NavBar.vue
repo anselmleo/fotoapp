5fff1a1f4d7f135afa38a9643295f52769428510
Anselm <anselmleo@gmail.com>
2022-01-04 14:08:11 +0100

    setup search navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,10 +2,13 @@
 <div>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <!-- <a class="navbar-brand" href="#">Search Photos</a> -->
+
+    <form class="form-inline my-2">
+      <input class="form-control-lg ml-2 mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="form-control-lg btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-md-auto gap-2">
         <li class="nav-item rounded">
@@ -17,7 +20,7 @@
         <li class="nav-item rounded">
           <a class="nav-link" href="#"><i class="bi bi-telephone-fill me-2"></i>Contact</a>
         </li>
-        <li class="nav-item dropdown rounded">
+        <!-- <li class="nav-item dropdown rounded">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-fill me-2"></i>Profile</a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">Account</a></li>
@@ -27,13 +30,13 @@
             </li>
             <li><a class="dropdown-item" href="#">Logout</a></li>
           </ul>
-        </li>
+        </li> -->
       </ul>
     </div>
   </div>
 </nav>
 </div>
-        
+
 </template>
 
 <style scoped>
